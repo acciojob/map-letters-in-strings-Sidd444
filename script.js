34c21e37b2bdb150506971b45182cceba91fd0cf
Sidd444 <123827703+Sidd444@users.noreply.github.com>
2024-01-19 22:42:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function mapLetters(s){
+	let obj={};
+	for(let i=0;i<s.length;i++){
+		if(!obj.hasOwnProperty(s[i])) obj.s[i]=[];
+		obj.s[i].push(i);
+	}
+	return obj;
+}
